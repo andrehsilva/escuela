0ea68ae69a02bdd134c6951c1b3e1434058c4546
andrehsilva <andrehrsilva@gmail.com>
2024-01-20 08:35:18 -0300

    continue

--- school/models.py
+++ school/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from tenant.models import Tenant
+import hashlib
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -11,6 +14,16 @@ class School(models.Model):
     description = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     activated = models.BooleanField(default=True)
+    hash_value = models.CharField(max_length=16, blank=True, null=True, unique=True)
+
+@receiver(pre_save, sender=School)
+def generate_hash(sender, instance, **kwargs):
+    if not instance.hash_value:
+        # Gera um hash usando o cnpj como base
+        hash_input = instance.cnpj.encode('utf-8')
+        hash_result = hashlib.md5(hash_input).hexdigest()[:16]
+        instance.hash_value = hash_result
+
 
 
     # Relacionamento com Tenant
--- tenant/models.py
+++ tenant/models.py
@@ -1,8 +1,22 @@
 from django.db import models
+import hashlib
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 
 class Tenant(models.Model):
     name = models.CharField(max_length=255, verbose_name="Nome do grupo escolar.")
     email = models.EmailField(unique=True, verbose_name="E-mail de contato oficial do grupo escolar.")
+    created_at = models.DateTimeField(auto_now_add=True)
+    activated = models.BooleanField(default=True)
+    hash_value_tenant = models.CharField(max_length=16, blank=True, null=True, unique=True)
+
+@receiver(pre_save, sender=Tenant)
+def generate_tenant_hash(sender, instance, **kwargs):
+    if not instance.hash_value_tenant:
+        # Gera um hash usando o email como base
+        hash_input = instance.email.encode('utf-8')
+        hash_result = hashlib.md5(hash_input).hexdigest()[:16]
+        instance.hash_value_tenant = hash_result
 
     def __str__(self):
         return str(self.name)
